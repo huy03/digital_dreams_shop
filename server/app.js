da50bbf5ec2871906485141367da09db9f74afb4
Hi <huydao1008@gmail.com>
2023-10-19 13:47:24 +0700

    config server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const userRouter = require("./routes/userRoute");
+const productRouter = require("./routes/productRoute");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
 
 module.exports = app;
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,11 +2,35 @@ const Product = require("./../models/product");
 
 exports.getAllProducts = async (req, res) => {
   try {
-    const products = await Product.find();
+    //Build query
+    //FILTERING
+    const queryObject = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((element) => delete queryObject[element]);
 
+    let queryString = JSON.stringify(queryObject);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+
+    let query = Product.find(JSON.parse(queryString));
+
+    //SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    //Execute query
+    const products = await query;
+
+    //Send response
     res.status(200).json({
       status: "success",
-      results: Product.length,
+      results: products.length,
       data: {
         products,
       },
@@ -19,12 +43,7 @@ exports.getAllProducts = async (req, res) => {
   }
 };
 
-exports.createProduct = async (req, res) => {
-  try {
-  } catch (e) {}
-};
-
-exports.getProductById = async (req, res) => {
+exports.getProduct = async (req, res) => {
   try {
     const product = await Product.findById(req.params.id);
 
@@ -40,4 +59,44 @@ exports.getProductById = async (req, res) => {
       message: e,
     });
   }
+};
+
+exports.createProduct = async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        product: newProduct,
+      },
+    });
+  } catch (e) {}
+};
+
+exports.updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        product,
+      },
+    });
+  } catch (e) {}
+};
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    await Product.findByIdAndUpdate(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (e) {}
 };
