6a3dbd2108209c7448655a66d105c7732bafc738
Hi <huydao1008@gmail.com>
2023-10-29 11:38:35 +0700

    auth feature

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -3,12 +3,12 @@ import 'package:digital_dreams_shop/core/utils/typdefs.dart';
 abstract class AuthRepository {
   const AuthRepository();
 
-  ResultFuture<void> signInWithEmailAndPassword({
+  ResultFuture<void> logInWithEmailAndPassword({
     required String email,
     required String password,
   });
-  ResultFuture<void> signInWithGoogle();
-  ResultFuture<void> signInWithFacebook();
+  ResultFuture<void> logInWithGoogle();
+  ResultFuture<void> logInWithFacebook();
   ResultFuture<void> signUpWithEmailAndPassword(String email, String password);
   ResultFuture<void> signOut();
   ResultFuture<bool> isSignedIn();
