36ebb49d5c0d4126cf7de9cfa9679c4d651dcd26
Hi <huydao1008@gmail.com>
2023-12-07 22:41:42 +0700

    update android

--- lib/core/constraints/constraints.dart
+++ lib/core/constraints/constraints.dart
@@ -1,6 +1,6 @@
 import 'package:intl/intl.dart';
 
-const String kBaseUrl = 'http://172.172.18.0:3000/api/v1';
+const String kBaseUrl = 'http://172.172.19.38:3000/api/v1';
 
 const String kAuthToken = 'AUTH_TOKEN';
 
