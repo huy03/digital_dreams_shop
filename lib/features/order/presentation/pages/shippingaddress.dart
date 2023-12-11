b6b73933eac7c6090aa6a0f9c34d29f89f95f198
chip267 <tranthanhhien123bt@gmail.com>
2023-12-11 15:45:36 +0700

    address

--- lib/features/order/presentation/pages/shippingaddress.dart
+++ lib/features/order/presentation/pages/shippingaddress.dart
@@ -2,6 +2,7 @@ import 'package:digital_dreams_shop/config/theme/colors.dart';
 import 'package:digital_dreams_shop/config/theme/media_resource.dart';
 import 'package:digital_dreams_shop/core/common/widgets/custom_button.dart';
 import 'package:digital_dreams_shop/features/order/presentation/widgets/address_widgets.dart';
+import 'package:digital_dreams_shop/features/order/presentation/widgets/bottom_container_widget.dart';
 import 'package:digital_dreams_shop/features/profile/presentation/widgets/text_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -70,44 +71,17 @@ class ShippingAddress extends StatelessWidget {
                     borderRadius: BorderRadius.circular(8),
                   ),
                 ),
-                onPressed: () {},
+                onPressed: () => showModalBottomSheet(
+                  isScrollControlled: true,
+                  context: context,
+                  builder: (context) => buildSheet(),
+                ),
                 child: Text(
                   'Add Address',
                   style: GoogleFonts.poppins(
                     fontSize: 14,
                     color: AppColor.bodyText,
                     fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: AppColor.background,
-                  boxShadow: [
-                    BoxShadow(
-                        color: const Color(0xff464646).withOpacity(0.1),
-                        offset: const Offset(
-                          0,
-                          -4,
-                        ),
-                        blurRadius: 24.0,
-                        spreadRadius: 0)
-                  ],
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(24),
-                    topRight: Radius.circular(24),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 30,
-                    vertical: 30,
-                  ),
-                  child: Column(
-                    children: [
-                      TextFormFieldSetting(content: 'Name'),
-                    ],
                   ),
                 ),
               ),
@@ -118,3 +92,69 @@ class ShippingAddress extends StatelessWidget {
     );
   }
 }
+
+// Widget buildSheet() => Column(mainAxisSize: MainAxisSize.min, children: [
+//       Container(
+//         width: double.infinity,
+//         decoration: BoxDecoration(
+//           color: AppColor.background,
+//           boxShadow: [
+//             BoxShadow(
+//                 color: const Color(0xff464646).withOpacity(0.1),
+//                 offset: const Offset(
+//                   0,
+//                   -4,
+//                 ),
+//                 blurRadius: 24.0,
+//                 spreadRadius: 0)
+//           ],
+//           borderRadius: const BorderRadius.only(
+//             topLeft: Radius.circular(24),
+//             topRight: Radius.circular(24),
+//           ),
+//         ),
+//         child: Padding(
+//           padding: const EdgeInsets.symmetric(
+//             horizontal: 30,
+//             vertical: 30,
+//           ),
+//           child: Padding(
+//             padding: const EdgeInsets.only(top: 31),
+//             child: Column(
+//               children: [
+//                 TextFormFieldSetting(content: 'Name'),
+//                 const SizedBox(
+//                   height: 15,
+//                 ),
+//                 TextFormFieldSetting(content: 'Phone number'),
+//                 const SizedBox(
+//                   height: 15,
+//                 ),
+//                 TextFormFieldSetting(content: 'Street'),
+//                 const SizedBox(
+//                   height: 15,
+//                 ),
+//                 TextFormFieldSetting(content: 'City'),
+//                 const SizedBox(
+//                   height: 15,
+//                 ),
+//                 TextFormFieldSetting(content: 'Distric'),
+//                 const SizedBox(
+//                   height: 15,
+//                 ),
+//                 TextFormFieldSetting(content: 'Country'),
+//                 const SizedBox(
+//                   height: 30,
+//                 ),
+//                 CustomButton(
+//                   width: double.infinity,
+//                   height: 45,
+//                   text: 'Save',
+//                   onPressed: () {},
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     ]);
