321a50620ef1a2eb4088d79fda685c401f8f8c18
Hi <huydao1008@gmail.com>
2023-12-11 17:08:10 +0700

    update text form field

--- lib/features/profile/presentation/pages/settings.dart
+++ lib/features/profile/presentation/pages/settings.dart
@@ -4,18 +4,48 @@ import 'package:digital_dreams_shop/config/theme/media_resource.dart';
 import 'package:digital_dreams_shop/core/common/widgets/custom_button.dart';
 import 'package:digital_dreams_shop/features/cart/presentation/cubit/cart_cubit.dart';
 import 'package:digital_dreams_shop/features/home/presentation/widgets/custom_suffix_icon.dart';
+import 'package:digital_dreams_shop/features/profile/presentation/cubit/profile_cubit.dart';
 import 'package:digital_dreams_shop/features/profile/presentation/widgets/text_form_field.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SettingProfile extends StatelessWidget {
+class SettingProfile extends StatefulWidget {
   const SettingProfile({super.key});
 
   @override
+  State<SettingProfile> createState() => _SettingProfileState();
+}
+
+class _SettingProfileState extends State<SettingProfile> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    phoneController.dispose();
+    super.dispose();
+  }
+
+  void submit() {
+    final isValidate = _formKey.currentState!.validate();
+    if (isValidate) {}
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final user = (context.watch<ProfileCubit>().state as ProfileLoaded).user;
+    nameController.value = TextEditingValue(text: user.userName);
+    emailController.value = TextEditingValue(text: user.email);
+    phoneController.value = TextEditingValue(text: user.phoneNumber);
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -23,89 +53,121 @@ class SettingProfile extends StatelessWidget {
             horizontal: 30,
             vertical: 24,
           ),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    SizedBox(
-                      height: 45,
-                      width: 45,
-                      child: IconButton(
-                        onPressed: () {
-                          context.pop();
-                        },
-                        icon: SvgPicture.asset(MediaResource.arrowBack),
-                        style: IconButton.styleFrom(
-                            backgroundColor: AppColor.primary, elevation: 2),
-                      ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  SizedBox(
+                    height: 45,
+                    width: 45,
+                    child: IconButton(
+                      onPressed: () {
+                        context.pop();
+                      },
+                      icon: SvgPicture.asset(MediaResource.arrowBack),
+                      style: IconButton.styleFrom(
+                          backgroundColor: AppColor.primary, elevation: 2),
                     ),
-                    const SizedBox(
-                      width: 20,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    'Settings',
+                    style: GoogleFonts.poppins(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                      color: AppColor.text,
                     ),
-                    Text(
-                      'Settings',
-                      style: GoogleFonts.poppins(
-                        fontSize: 23,
-                        fontWeight: FontWeight.bold,
-                        color: AppColor.text,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Center(
-                  child: Column(
-                    children: [
-                      Container(
-                        width: 90,
-                        height: 90,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          image: DecorationImage(
-                            image: NetworkImage(
-                                'https://image.kpopmap.com/2020/10/blackpink-lisa-mac-2.jpg'),
-                            fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 90,
+                          height: 90,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            image: DecorationImage(
+                              image: NetworkImage(user.avatar!),
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      TextFormFieldSetting(content: 'Name'),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      TextFormFieldSetting(content: 'Email'),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      TextFormFieldSetting(content: 'Phone number'),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      TextFormFieldSetting(content: 'Gender'),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      TextFormFieldSetting(content: 'Date of Birth'),
-                      const SizedBox(
-                        height: 45,
-                      ),
-                      CustomButton(
-                        width: double.infinity,
-                        height: 45,
-                        text: 'Save',
-                        onPressed: () {},
-                      ),
-                    ],
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        TextFormFieldSetting(
+                          controller: nameController,
+                          hintText: 'Username',
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        TextFormFieldSetting(
+                          controller: emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          hintText: 'Email',
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Email is required!';
+                            }
+                            if (!EmailValidator.validate(value)) {
+                              return 'Please enter a valid email!';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        TextFormFieldSetting(
+                          controller: phoneController,
+                          keyboardType: TextInputType.phone,
+                          hintText: 'Phone Number',
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Phone number is required!';
+                            }
+                            var regExp = RegExp(r'^(?:[+0][1-9])?[0-9]{10}$');
+                            if (!regExp.hasMatch(value)) {
+                              return 'Please enter a valid phone number!';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        TextFormFieldSetting(hintText: 'Gender'),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        TextFormFieldSetting(hintText: 'Date of Birth'),
+                        const SizedBox(
+                          height: 45,
+                        ),
+                        CustomButton(
+                          width: double.infinity,
+                          height: 45,
+                          text: 'Save',
+                          onPressed: submit,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
